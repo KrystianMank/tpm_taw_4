724570d120a614d8966514f4205f956e9dbf716f
KrystianMank <man-kry-2005@wp.pl>
2023-09-14 13:44:48 +0200

    Operatory matematyczne

---
+++ operatory.py
@@ -0,0 +1,26 @@
+'''
+Prosty kalkulator ver. 0.1
+K>M> 2023
+'''
+#pobieranie danych
+
+x=float(input("Podaj x="))
+y=float(input("Podaj y="))
+
+#obliczenia
+suma=x + y
+roznica = x- y
+iloczyn = x * y
+iloraz= x / y
+
+potega = x ** y
+pierwiastek = x ** 0.5
+
+czesc_calkowita_dzielenia = x // y
+reszta_z_dzielenia = x % y
+
+print(f"Suma = {suma}\n rożnica = {roznica}\n \
+    iloczyn = {iloczyn}\n iloraz = {iloraz}\n \
+        potega = {potega} pierwaistek = {pierwiastek}\n \
+            cześć całkowita z dzieleniia = {czesc_calkowita_dzielenia}\n \
+                reszta z dzielenia = {reszta_z_dzielenia}")
