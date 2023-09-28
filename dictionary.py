ed3b58c2284f04ca9e77f851a2902aad3e1c0e30
KrystianMank <man-kry-2005@wp.pl>
2023-09-28 14:14:34 +0200

    Slowniki

---
+++ dictionary.py
@@ -0,0 +1,44 @@
+'''
+Słowniki
+'''
+dict1={'imie':'Anna','wiek':18,'wzrost':1.67}
+# print(type(dict1))
+# print(dict1)
+
+# sl2=dict()
+# print(type(sl2))
+
+# secik=set()
+# print(type(secik))
+
+# print(f"Slownik: {dict1.items()}")
+
+# for key,value in dict1.items():
+#     print(f"{key} - {value}")
+
+# print(f"KLucze: {dict1.keys()}")
+# print(f"KLucze: {dict1.values()}")
+
+# print(dict1['imie'])
+# print(dict1.get('imie'))
+
+# if 'cos' in dict1.keys():
+#     print(dict1['cos'])
+# else:
+#     print("Klucz nie istnieje")
+    
+# dict1.pop('wiek')
+# print(dict1)
+
+# dict1['wiek']=18
+# print(dict1)
+
+# lista=list(dict1.items())
+# lista[1],lista[2]=lista[2],lista[1]
+# dict1=dict(lista)
+# print(dict1)
+
+dict1.update({'zawod':'uczen'})
+print(dict1)
+
+ 
